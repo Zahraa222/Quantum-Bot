b2d9891ac27c85cf372a00ee7254efa534b5d587
Zahraa <zzmmk2003@gmail.com>
2024-05-27 18:23:27 -0400

    api endpoint creation

--- functions/api/addMessage.js
+++ functions/api/addMessage.js
@@ -1,41 +1,43 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { text } = require("body-parser");
-const {logger} = require(functions);
+const {logger} = functions;
 
 exports.addMessage = functions.https.onCall(async (data, context) => {
-    try{
-        logger.log("Received data", data);
+  try {
+    logger.log("Received data", data);
 
-        if(!data.text || !data.userId){
-            logger.log("Requred fields (text or userId) are missing");
-            throw new functions.https.HttpsError("invalid-argument", "The required fields are (text or user Id) are missing");
-        }
-
-
-        const {text, userId} = data;
-        //construct message data
-        const messageData = {
-            text,
-            userId,
-            timestamp: admin.firestore.FieldValue.serverTimestamp()
-        };
-
-        //add message to the user's message subcollection in firestore
-        const messageRef = await admin.firestore().collection("chats").doc(userId).collection("messages").add(messageData);
-        logger.log("Message added, message ID: ", messageRef.id);
-
-
-        //return success status and message ID
-        return {
-            status: "success",
-            messageId: messageRef.id
-        };
-    
-
-    } catch (error) {
-        logger.error("Error adding message: ", error);
-        //throw error to client
-        throw new functions.https.HttpsError("unknown", "An error occurred while adding the message", error.message);
+    if (!data.text || !data.userId) {
+      logger.log("Requred fields (text or userId) are missing");
+      throw new functions.https.HttpsError("invalid-argument", "text/userId");
     }
+
+
+    const {text, userId} = data;
+
+    // construct message data
+    const messageData = {
+      text,
+      userId,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    // add message to the user's message subcollection in firestore
+    const messageRef = await admin.firestore()
+        .collection("chats")
+        .doc(userId)
+        .collection("messages")
+        .add(messageData);
+    logger.log("Message added, message ID: ", messageRef.id);
+
+
+    // return success status and message ID
+    return {
+      status: "success",
+      messageId: messageRef.id,
+    };
+  } catch (error) {
+    logger.error("Error adding message: ", error);
+    // throw error to client
+    throw new functions.https.HttpsError("unknown", "error", error.message);
+  }
 });
